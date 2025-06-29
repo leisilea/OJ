214fba4dd96bb2149fbb6c5113da2815c81e18f4
leisilea <2608836364@qq.com>
2025-06-29 13:22:46 +0800

    成功搭建vue-cli

---
+++ oj_cil_f/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <BasicLayout />
+  </div>
+</template>
+<!-- 
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template> -->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+<script>
+import { defineComponent } from "vue";
+import BasicLayout from "@/layouts/BasicLayout.vue"; // @ is an alias to /src
+export default defineComponent({
+  name: "App",
+  components: {
+    BasicLayout,
+  },
+});
+</script>
---
+++ oj_cil_f/src/layouts/BasicLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="basic-layout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">导航栏</a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/leisilea/OJ" target="_blank" rel="noopener"
+          >gz_oj_test</a
+        >
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
